1cd4470ebc79f67157ddae76a4f2c26b88982bab
Afonso Ambrósio <afonsoabsm@gmail.com>
2022-09-18 12:07:32 -0300

    full project

---
+++ web/_php/firebase.php
@@ -0,0 +1,12 @@
+<?php
+require __DIR__.'/vendor/autoload.php';
+
+use Kreait\Firebase\Factory;
+
+$factory = (new Factory)->withServiceAccount('/opt/sennatempmonitor/firebase_credentials.json');
+
+$database = $factory->createDatabase();
+
+$esp0 = $database->getReference('esp0')->getSnapshot();
+
+file_put_contents('/var/www/html/weather-monitor/data.json', json_encode($esp0->getValue()));
