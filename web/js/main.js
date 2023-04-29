f590ec9ce3d45331cf65badc624f970012caea9a
Canudo <afonsoabsm@gmail.com>
2023-04-28 22:49:56 -0300

    chart now shows humidity and has more details

--- web/js/main.js
+++ web/js/main.js
@@ -3,8 +3,10 @@ var bar = document.getElementsByClassName("btn");
 var last_t = document.getElementById("last_temperature");
 var last_h = document.getElementById("last_humidity");
 var last_d = document.getElementById("last_measuring");
-var min_t = document.getElementById("min_temperature");
-var max_t = document.getElementById("max_temperature");
+var min_temperature = document.getElementById("min_temperature");
+var min_temperature = document.getElementById("min_temperature");
+var min_humidity = document.getElementById("min_humidity");
+var max_humidity = document.getElementById("max_humidity");
 
 var measurings = new Object();
 var last;
@@ -24,7 +26,7 @@ loadData().then((data) => {
     last = measurings.slice(-1);
     let date = new Date(last[0][0] * 1000);
     
-    last_d.textContent = (dateFormat(date) + " às " + timeFormat(date));
+    last_d.textContent = (dateFormat(date) + " at " + timeFormat(date));
     last_t.textContent = (last[0][1].t.slice(0,4) + '°C');
     last_h.textContent = (last[0][1].h.slice(0,4) + '%');
 
@@ -52,7 +54,7 @@ function today(){
         start_date = new Date();
         start_date.setHours(0,0,0,0);
 
-        plot('Hoje');
+        plot('Today');
 
     }
 }
@@ -64,7 +66,7 @@ function last24Hours(){
         start_date = new Date();
         start_date.setDate(start_date.getDate() - 1);
 
-        plot('Ultimas 24 horas');
+        plot('Last 24 hours');
     }
 }
 
@@ -75,7 +77,7 @@ function last7Days(){
         start_date = new Date();
         start_date.setDate(start_date.getDate() - 7);
 
-        plot('Ultimos 7 dias');
+        plot('Last 7 days');
     }
 }
 
@@ -86,59 +88,81 @@ function last30Days(){
         start_date = new Date();
         start_date.setDate(start_date.getDate() - 30);
 
-        plot('Ultimos 30 dias');
+        plot('Last 30 days');
     }
 }
 
 function plot(title){
+    var min_h = last[0];
+    var max_h = last[0];
+    var min_t = last[0];
+    var max_t = last[0];
     
-
-    var min = last[0];
-    var max = last[0];
-
-    var chartData = [["hora", "temperatura"]];
-
-    measurings.forEach((item) => {
-        if((item[0] * 1000) >= (start_date.getTime() - 1800)){
-            let chartHour = new Date(item[0] * 1000);
-            
-            let hour = ((measurings.length < 76) ? timeFormat(chartHour) : dateFormat(chartHour) + ' ' + timeFormat(chartHour));
-            
-            chartData.push([hour, parseFloat(item[1].t)]);
-
-            if(parseFloat(item[1].t) < parseFloat(min[1].t)) min = item;
-            if(parseFloat(item[1].t) > parseFloat(max[1].t)) max = item;
-        }
-    });
-
-    if(chartData.length < 2) return false;
-
-    var min_h = new Date(min[0] * 1000);
-    min_t.innerHTML = 'min <span class="badge bg-info text-dark">' + min[1].t.slice(0,4) + '°C</span> dia ' + dateFormat(min_h) + ' às ' + timeFormat(min_h);
-    var max_h = new Date(max[0] * 1000);
-    max_t.innerHTML = 'max <span class="badge bg-warning text-dark">' + max[1].t.slice(0,4) + '°C</span> dia ' + dateFormat(max_h) + ' às ' + timeFormat(max_h);
-
-    google.charts.load('current', {'packages':['corechart']});
+    google.charts.load('current', {'packages':['corechart']})
     google.charts.setOnLoadCallback(drawChart);
-
+    
     function drawChart() {
+        var data = new google.visualization.DataTable();
+        var date_formatter = new google.visualization.DateFormat({ pattern: 'dd/MM HH:mm' });
+        
+        data.addColumn('date', 'time');
+        data.addColumn('number', 'temperature');
+        data.addColumn('number', 'humidity');
 
-        var data = google.visualization.arrayToDataTable(chartData);
+        measurings.forEach((item) => {
+            if((item[0] * 1000) >= start_date.getTime()){
+                let hour = new Date(item[0] * 1000);
+
+                //let hour = ((measurings.length < 76) ? timeFormat(chartHour) : dateFormat(chartHour) + ' ' + timeFormat(chartHour));
+
+                data.addRow([hour, parseFloat(item[1].t), parseFloat(item[1].h)]);
+
+                //data.push([hour, parseFloat(item[1].t), parseFloat(item[1].h)]);
+
+                if(parseFloat(item[1].t) < parseFloat(min_t[1].t)) min_t = item;
+                if(parseFloat(item[1].t) > parseFloat(max_t[1].t)) max_t = item;
+
+                if(parseFloat(item[1].h) < parseFloat(min_h[1].h)) min_h = item;
+                if(parseFloat(item[1].h) > parseFloat(max_h[1].h)) max_h = item;
+            }
+        });
+        
+        date_formatter.format(data, 0);
+        
+        let min_t_time = new Date(min_t[0] * 1000);
+        min_temperature.innerHTML = 'min <span class="badge bg-info text-dark">' + min_t[1].t.slice(0,4) + '°C</span> on ' + dateFormat(min_t_time) + ' at ' + timeFormat(min_t_time);
+        var max_t_time = new Date(max_t[0] * 1000);
+        max_temperature.innerHTML = 'max <span class="badge bg-warning text-dark">' + max_t[1].t.slice(0,4) + '°C</span> on ' + dateFormat(max_t_time) + ' at ' + timeFormat(max_t_time);
+
+        let min_h_time = new Date(min_t[0] * 1000);
+        min_humidity.innerHTML = 'min <span class="badge bg-info text-dark">' + min_h[1].h.slice(0,4) + '%</span> on ' + dateFormat(min_h_time) + ' at ' + timeFormat(min_h_time);
+        var max_h_time = new Date(max_t[0] * 1000);
+        max_humidity.innerHTML = 'max <span class="badge bg-warning text-dark">' + max_h[1].h.slice(0,4) + '%</span> on ' + dateFormat(max_h_time) + ' at ' + timeFormat(max_h_time);
 
         var options = {
-            title: title,
-            pointSize: 3,
-            legend: {
-                position: 'bottom'
+            title: 'Temperature and Humidity - ' + title,
+            height: 400,
+            chartArea: {width: '90%'},
+            legend: {position: 'bottom'},
+            series: {
+                0: {targetAxisIndex: 0},
+                1: {targetAxisIndex: 1}
             },
-            chartArea: {
-                left: 50,
-                width: '100%'
+            vAxes: {
+                // Adds titles to each axis.
+                0: {title: 'Temperature (ºC)'},
+                1: {title: 'Humidity (%)'}
+            },
+            hAxis: {
+                format: (data.getNumberOfRows() < 75) ? 'HH:mm' : 'dd/MM'
+            },
+            vAxis:{
+                textPosition: 'out'
             }
         };
 
-        var chart = new google.visualization.LineChart(document.getElementById('line_chart'));
-
+        var chart = new google.visualization.LineChart(document.getElementById('gchart'));
+        
         chart.draw(data, options);
     }
 }
